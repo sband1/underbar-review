13f6bacc5ff39c8a4017ffe14fdc72b3f8fb4f21
Travis Williams <williams44t@gmail.com>
2020-10-06 20:41:06 -0700

    Fix _.defaults tests

--- spec/part2.js
+++ spec/part2.js
@@ -278,7 +278,13 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        // throw new Error('This test is missing.');
+        var destination = { a: 1 };
+        var source = { a: 2 };
+
+        _.defaults(destination, source);
+
+        expect(destination.a).to.equal(1);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
