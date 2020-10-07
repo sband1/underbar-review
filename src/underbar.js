c409e078b658aa77471aba07ae569f1d364b1134
Travis Williams <williams44t@gmail.com>
2020-10-06 20:41:33 -0700

    Implement _.defaults fn

--- src/underbar.js
+++ src/underbar.js
@@ -259,6 +259,17 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var args = arguments;
+
+    for (var i = 1; i < arguments.length; i++) {
+
+      _.each(arguments[i], function(val, key) {
+        if (obj[key] === undefined) { obj[key] = val; }
+      });
+
+    }
+
+    return obj;
   };
 
 
